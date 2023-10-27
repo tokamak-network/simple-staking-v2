1db4f81d1c8efaa4e820655fac3efaca1859c43c
cd4761 <cd4761@gmail.com>
2023-10-27 09:25:14 +0900

    remove console

--- src/hooks/staking/useCandidateList.ts
+++ src/hooks/staking/useCandidateList.ts
@@ -17,7 +17,6 @@ export function useCandidateList () {
     
     async function fetch () {
       if (data) {
-        console.log('data',data)
         const candidates = await Promise.all(data.candidates.map(async (obj: any) => {
           let tempObj = obj
           if (
@@ -30,13 +29,10 @@ export function useCandidateList () {
             try{
               
               const stakeOf = await SeigManager_CONTRACT.stakeOf(obj.candidateContract, account)
-              console.log('stakeOf', stakeOf)
               const stakeOfCandidate = await SeigManager_CONTRACT.stakeOf(obj.candidateContract, obj.candidate)
-              console.log('stakeOfCandidate', stakeOfCandidate)
               const pending = await DepositManager_CONTRACT.pendingUnstakedLayer2(obj.candidateContract)
-              console.log('pending', pending)
               const old_pending = await Old_DepositManager_CONTRACT.pendingUnstakedLayer2(getOldLayerAddress(obj.candidateContract))
-              console.log('old_pending', old_pending)
+              
               const sumPending = pending.add(old_pending)
   
               tempObj = {
@@ -45,14 +41,12 @@ export function useCandidateList () {
                 pending: sumPending.toString(),
                 stakeOfCandidate: stakeOfCandidate.toString()
               }
-              console.log('tempObj', tempObj)
             } catch (e) {
               console.log(e)
             }
           }
           return tempObj
         }))
-        console.log('candidateList',candidateList)
         setCandidateList(candidates)
       }
     }
