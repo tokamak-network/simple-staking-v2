0931bf85dbf96a5b60f8fa40f65cd6e2aa4c0bae
cd4761 <cd4761@gmail.com>
2024-10-24 14:32:34 +0900

    update

--- src/pages/components/staking/CalculatorModal.tsx
+++ src/pages/components/staking/CalculatorModal.tsx
@@ -72,7 +72,6 @@ function CalculatorModal() {
       const unit = duration === '1-year' ? 365 : duration === '6-month' ? 30 : 7;
 
       const returnRate = calculateRoiBasedonCompound({ totalStakedAmount: total, totalSupply: totalSup, duration });
-      console.log(returnRate)
 
       const stakedRatio = total / totalSup;
       const compensatePeraDay = stakedRatio * maxCompensate;
