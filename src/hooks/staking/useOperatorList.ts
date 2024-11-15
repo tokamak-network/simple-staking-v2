6111be28d0de9d8e6b3741420582ca08511aaefb
SonYoungsung <sysgigigi@gmail.com>
2024-11-15 16:24:46 +0900

    feat: apply fetch-policy for custom hooks with Subgraph

--- src/hooks/staking/useOperatorList.ts
+++ src/hooks/staking/useOperatorList.ts
@@ -1,78 +1,79 @@
-import { getOperatorsInfo } from "@/api";
+import { getOperatorsInfo } from '@/api';
 import { useEffect, useState } from 'react';
-import { NON_CANDIDATE } from "@/constants";
+import { NON_CANDIDATE } from '@/constants';
 import { useWeb3React } from '@web3-react/core';
 import { convertNumber } from '@/utils/number';
 import useCallContract from '@/hooks/useCallContract';
 import { BigNumber } from 'ethers';
 import { useRecoilValue } from 'recoil';
 import { txState } from '@/atom/global/transaction';
-import { useWindowDimensions } from "../useWindowDimensions";
-// import { useCandidateList } from './useCandidateList';
-import { GET_CANDIDATE } from "@/graphql/getCandidates";
-import { useQuery } from "@apollo/client";
+import { useWindowDimensions } from '../useWindowDimensions';
 
 export function useOperatorList() {
   const [operatorList, setOperatorList] = useState([]);
-  const [userTotalStaked, setUserTotalStaked] = useState('0.00')
-  const [totalStaked, setTotalStaked] = useState<string>()
+  const [userTotalStaked, setUserTotalStaked] = useState('0.00');
+  const [totalStaked, setTotalStaked] = useState<string>();
   const { account } = useWeb3React();
   const { DepositManager_CONTRACT, SeigManager_CONTRACT } = useCallContract();
-  const tx = useRecoilValue(txState)
+  const tx = useRecoilValue(txState);
   const [width] = useWindowDimensions();
-  
+
   useEffect(() => {
     async function fetchList() {
       const data = await getOperatorsInfo();
-      
-      let staked = BigNumber.from('0')
-      let totalStake: BigNumber = BigNumber.from('0')
-      
-      const operators = await Promise.all(data.map(async (obj: any) => {
-        let pendingUnstakedLayer2 = ''
-        let stakeOf = '0'
-        let commisionRates = undefined
 
-        const pendingWithdrawal = pendingUnstakedLayer2 ? convertNumber({
-          amount: pendingUnstakedLayer2.toString(),
-          type: 'ray',
-          localeString: true,
-        }) : '-'
-        
-        totalStake = obj.updateCoinageTotalString ? totalStake.add(obj.updateCoinageTotalString) : BigNumber.from('0')
-        staked = staked.add(stakeOf)
-       
-        const commissionRate = '-'
-        const find = NON_CANDIDATE.find(data => data.layer2 === obj.layer2)
-        const fetchedData = {
-          ...obj,
-          operatorsHistory: history,
-          delegators: 0,
-          commit: [],
-          pendingWithdrawal: pendingWithdrawal,
-          yourStaked: '0',
-          commissionRate: commissionRate,
-   
-        }
-        return find ?
-          await { ...fetchedData, name: find.name } : await fetchedData
-      }))
+      let staked = BigNumber.from('0');
+      let totalStake: BigNumber = BigNumber.from('0');
 
-      setTotalStaked(totalStake.toString())
-      setUserTotalStaked(staked.toString())
+      const operators = await Promise.all(
+        data.map(async (obj: any) => {
+          let pendingUnstakedLayer2 = '';
+          let stakeOf = '0';
+          let commisionRates = undefined;
+
+          const pendingWithdrawal = pendingUnstakedLayer2
+            ? convertNumber({
+                amount: pendingUnstakedLayer2.toString(),
+                type: 'ray',
+                localeString: true,
+              })
+            : '-';
+
+          totalStake = obj.updateCoinageTotalString
+            ? totalStake.add(obj.updateCoinageTotalString)
+            : BigNumber.from('0');
+          staked = staked.add(stakeOf);
+
+          const commissionRate = '-';
+          const find = NON_CANDIDATE.find((data) => data.layer2 === obj.layer2);
+          const fetchedData = {
+            ...obj,
+            operatorsHistory: history,
+            delegators: 0,
+            commit: [],
+            pendingWithdrawal: pendingWithdrawal,
+            yourStaked: '0',
+            commissionRate: commissionRate,
+          };
+          return find ? await { ...fetchedData, name: find.name } : await fetchedData;
+        }),
+      );
+
+      setTotalStaked(totalStake.toString());
+      setUserTotalStaked(staked.toString());
 
       if (operators) {
         operators.sort(function (a: any, b: any) {
-          return b.updateCoinageTotalString - a.updateCoinageTotalString
-        })
+          return b.updateCoinageTotalString - a.updateCoinageTotalString;
+        });
         //@ts-ignore
-        setOperatorList(operators)
+        setOperatorList(operators);
       }
     }
-    fetchList()
-  }, [])
+    fetchList();
+  }, []);
 
-  return { operatorList, userTotalStaked, totalStaked, tx }
+  return { operatorList, userTotalStaked, totalStaked, tx };
 }
 
-export default useOperatorList
+export default useOperatorList;
