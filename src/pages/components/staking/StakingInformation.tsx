1fa24b182891c3f9892dc89da28df0966178ffc9
jason hwang <cd4761@gmail.com>
2025-07-28 13:54:50 +0900

    restrict unstake & withdraw

--- src/pages/components/staking/StakingInformation.tsx
+++ src/pages/components/staking/StakingInformation.tsx
@@ -19,13 +19,15 @@ import { useWithdrawable } from "@/hooks/staking/useWithdrawable";
 import useUserBalance from "@/hooks/useUserBalance";
 import { StakeModalDataType } from "@/types";
 import { ModalType } from "@/types/modal";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Modal, ModalOverlay, ModalContent, ModalBody, Text, Button, Link } from "@chakra-ui/react";
 import { useWeb3React } from "@web3-react/core";
 import { FC, useCallback, useEffect, useMemo, useState } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import WalletInformation from "./WalletInformation";
 import { useWithdrawRequests } from "../../../hooks/staking/useWithdrawable";
 import { txState } from "@/atom/global/transaction";
+import CLOSE_ICON from "assets/images/popup-close-icon.svg";
+import Image from "next/image";
 
 type StakingInformationProps = {
 	// dispatch: AppDispatch;
@@ -42,6 +44,7 @@ export const StakingInformation: FC<StakingInformationProps> = ({ data }) => {
 	const [typeFilter, setTypeFilter] = useRecoilState(typeFilterState);
 	const [filteredTxHistory, setFilteredTxHistory] = useState(txHistory);
 	const [transactionTab, setTransactionTab] = useState(false);
+	const [showCommunityModal, setShowCommunityModal] = useState(false);
 
 	const [candidateContracts, setCandidateContracts] = useState("");
 	const [candidates, setCandidates] = useState("");
@@ -225,122 +228,183 @@ export const StakingInformation: FC<StakingInformationProps> = ({ data }) => {
 
 	const modalButton = useCallback(
 		async (modalType: ModalType, data: any) => {
-			setSelectedModal(modalType);
-			setSelectedModalData(data);
+			if (modalType === "withdraw") {
+				setShowCommunityModal(true);
+			} else {
+				setSelectedModal(modalType);
+				setSelectedModalData(data);
+			}
 		},
 		[candidateContracts],
 	);
 
 	return (
-		<Flex
-			w="960px"
-			m={0}
-			justifyContent={"center"}
-			alignItems="center"
-			// pt="70px"
-			border={"none"}
-			flexDir={"column"}
-			ml={"70px"}
-		>
-			<Flex alignItems={"center"} justifyContent={"center"} w={"960px"}>
-				<Box p={0} borderRadius={"10px"} alignSelf={"flex-start"}>
-					<WalletInformation data={data} commitHistory={commitHistory} />
-				</Box>
+		<>
+			<Flex
+				w="960px"
+				m={0}
+				justifyContent={"center"}
+				alignItems="center"
+				// pt="70px"
+				border={"none"}
+				flexDir={"column"}
+				ml={"70px"}
+			>
+				<Flex alignItems={"center"} justifyContent={"center"} w={"960px"}>
+					<Box p={0} borderRadius={"10px"} alignSelf={"flex-start"}>
+						<WalletInformation data={data} commitHistory={commitHistory} />
+					</Box>
+					<Flex
+						flexDir={"row"}
+						alignItems={"space-between"}
+						ml={"45px"}
+						w={"585px"}
+					>
+						<OperatorDetailInfo
+							title={"Staked"}
+							value={data?.stakeOf}
+							totalValue={data?.stakedAmount}
+							unit={"TON"}
+							type={""}
+							dataModal={() => modalButton("withdraw", dataModal)}
+						/>
+						<OperatorDetailInfo
+							title={"Unclaimed Staking Reward"}
+							value={expectedSeig}
+							totalValue={seigOfLayer}
+							unit={"TON"}
+							type={""}
+							contractInfo={data?.candidateContract}
+							candidate={data?.candidate}
+							minimumAmount={minimumAmount}
+						/>
+						<OperatorDetailInfo
+							title={"Pending Withdrawal"}
+							value={data?.myPending}
+							totalValue={data?.pending}
+							unit={"TON"}
+							type={""}
+							dataModal={() => modalButton("restake", dataModal)}
+						/>
+					</Flex>
+				</Flex>
 				<Flex
 					flexDir={"row"}
-					alignItems={"space-between"}
-					ml={"45px"}
-					w={"585px"}
-				>
-					<OperatorDetailInfo
-						title={"Staked"}
-						value={data?.stakeOf}
-						totalValue={data?.stakedAmount}
-						unit={"TON"}
-						type={""}
-						dataModal={() => modalButton("withdraw", dataModal)}
-					/>
-					<OperatorDetailInfo
-						title={"Unclaimed Staking Reward"}
-						value={expectedSeig}
-						totalValue={seigOfLayer}
-						unit={"TON"}
-						type={""}
-						contractInfo={data?.candidateContract}
-						candidate={data?.candidate}
-						minimumAmount={minimumAmount}
-					/>
-					<OperatorDetailInfo
-						title={"Pending Withdrawal"}
-						value={data?.myPending}
-						totalValue={data?.pending}
-						unit={"TON"}
-						type={""}
-						dataModal={() => modalButton("restake", dataModal)}
-					/>
-				</Flex>
-			</Flex>
-			<Flex
-				flexDir={"row"}
-				h={"30px"}
-				justifyContent={"center"}
-				alignItems={"center"}
-				my={"30px"}
-			>
-				<Flex
-					w={"150px"}
-					h={"1px"}
-					background={
-						"linear-gradient(90deg, rgba(231, 235, 242, 0.00) 0%, #E7EBF2 100%)"
-					}
-				/>
-				<Flex
-					borderRadius={"14px"}
-					border={"1px solid #e7ebf2"}
-					w={"100px"}
-					h={"25px"}
-					fontSize={"12px"}
-					fontWeight={500}
-					color={"#304156"}
+					h={"30px"}
 					justifyContent={"center"}
 					alignItems={"center"}
-					onClick={() => setTransactionTab(transactionTab ? false : true)}
-					cursor={"pointer"}
+					my={"30px"}
 				>
-					{transactionTab ? "Close" : "Transactions"}
+					<Flex
+						w={"150px"}
+						h={"1px"}
+						background={
+							"linear-gradient(90deg, rgba(231, 235, 242, 0.00) 0%, #E7EBF2 100%)"
+						}
+					/>
+					<Flex
+						borderRadius={"14px"}
+						border={"1px solid #e7ebf2"}
+						w={"100px"}
+						h={"25px"}
+						fontSize={"12px"}
+						fontWeight={500}
+						color={"#304156"}
+						justifyContent={"center"}
+						alignItems={"center"}
+						onClick={() => setTransactionTab(transactionTab ? false : true)}
+						cursor={"pointer"}
+					>
+						{transactionTab ? "Close" : "Transactions"}
+					</Flex>
+					<Flex
+						w={"150px"}
+						h={"1px"}
+						background={
+							"linear-gradient(90deg, #E7EBF2 0%, rgba(231, 235, 242, 0.00) 100%)"
+						}
+					/>
 				</Flex>
-				<Flex
-					w={"150px"}
-					h={"1px"}
-					background={
-						"linear-gradient(90deg, #E7EBF2 0%, rgba(231, 235, 242, 0.00) 100%)"
-					}
-				/>
+				{/* table area */}
+				{transactionTab ? (
+					<Flex flexDir={"row"} justifyContent={"center"} alignItems={"center"}>
+						{commitHistory && (
+							<HistoryTable
+								columns={historyColumns}
+								data={commitHistory}
+								tableType={"Update Seigniorage"}
+								dataModal={dataModal}
+							/>
+						)}
+						{filteredTxHistory && (
+							<HistoryTable
+								columns={historyColumns}
+								data={filteredTxHistory}
+								tableType={"Transactions"}
+								dataModal={dataModal}
+							/>
+						)}
+					</Flex>
+				) : (
+					""
+				)}
 			</Flex>
-			{/* table area */}
-			{transactionTab ? (
-				<Flex flexDir={"row"} justifyContent={"center"} alignItems={"center"}>
-					{commitHistory && (
-						<HistoryTable
-							columns={historyColumns}
-							data={commitHistory}
-							tableType={"Update Seigniorage"}
-							dataModal={dataModal}
-						/>
-					)}
-					{filteredTxHistory && (
-						<HistoryTable
-							columns={historyColumns}
-							data={filteredTxHistory}
-							tableType={"Transactions"}
-							dataModal={dataModal}
-						/>
-					)}
-				</Flex>
-			) : (
-				""
-			)}
-		</Flex>
+
+			
+			{/* 커뮤니티 버전 안내 모달 */}
+			<Modal isOpen={showCommunityModal} onClose={() => setShowCommunityModal(false)} isCentered>
+				<ModalOverlay />
+				<ModalContent 
+					bg={"#fff"} 
+					w={"400px"} 
+					borderRadius={"15px"} 
+					boxShadow={"0 2px 6px 0 rgba(61, 73, 93, 0.1)"}
+				>
+					<ModalBody py={"30px"} pos={"relative"}>
+						<Flex
+							pos={"absolute"}
+							right={"-30px"}
+							top={"-30px"}
+							cursor={"pointer"}
+							onClick={() => setShowCommunityModal(false)}
+						>
+							<Image src={CLOSE_ICON} alt={"CLOSE_ICON"} />
+						</Flex>
+						<Flex flexDir={"column"} alignItems={"center"} textAlign={"center"}>
+							<Text 
+								color={"#3d495d"} 
+								fontSize={"18px"} 
+								fontWeight={"bold"} 
+								mb={"15px"}
+							>
+								Tokamak Staking is transitioning to the Community Version.
+							</Text>
+							<Text 
+								color={"#86929d"} 
+								fontSize={"14px"} 
+								mb={"25px"}
+							>
+								Unstake & Withdraw are available on Community Version.
+							</Text>
+							<Link href={`https://community.staking.tokamak.network/${candidateContracts}`} isExternal>
+								<Button
+									bg={"#2a72e5"}
+									color={"white"}
+									_hover={{ bg: "#1e5bb8" }}
+									w={"200px"}
+									h={"40px"}
+									borderRadius={"8px"}
+									fontSize={"14px"}
+									fontWeight={500}
+								>
+									Go to Community Version
+								</Button>
+							</Link>
+						</Flex>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+		</>
 	);
 };
 
--- src/pages/components/staking/mobile/MobileWithdraw.tsx
+++ src/pages/components/staking/mobile/MobileWithdraw.tsx
@@ -1,4 +1,4 @@
-import { Flex, useDisclosure } from "@chakra-ui/react";
+import { Flex, useDisclosure, Text, Link, Box } from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
 import { WithdrawType } from "../../../../common/modal/withdraw/WithdrawType";
 // import TITAN from '@/assets/images/titan_symbol.svg'
@@ -20,11 +20,6 @@ export function MobileWithdraw(args: MobileWithdrawProps) {
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const [type, setType] = useState("");
 	// const [selectedOp, setSelectedOp] = useState<any>(undefined);
-
-	const openDrawer = (type: string) => {
-		onOpen();
-		setType(type);
-	};
 
 	const staked = selectedOp?.stakeOf
 		? convertNumber({
@@ -75,37 +70,52 @@ export function MobileWithdraw(args: MobileWithdrawProps) {
 					/>
 					<UnstakableBalance stakedAmount={staked} justify={"start"} />
 				</Flex>
-				{selectedOp && selectedOp.candidateAddOn !== null ? (
-					<Flex flexDir={"column"}>
-						<WithdrawType
-							name={"Withdraw to Ethereum"}
-							content={
-								"Staked TON can be unstaked and can be withdrawn after 93,046 blocks from unstaking (~14 days)."
-							}
-							src={ETHEREUM.src}
-							onClick={() => openDrawer("Ethereum")}
-						/>
-						<WithdrawType
-							name={"Withdraw to L2"}
-							content={
-								"Instead of withdrawing to Ethereum, staked TON can be withdrawn to this layer as TON. By withdrawing to this layer, TON can be used right away without needing to wait for 14 days."
-							}
-							src={logo ? logo : "L2"}
-							onClick={() => openDrawer("Titan")}
-						/>
+				
+				<Box
+					px={"20px"}
+					py={"15px"}
+					w={"100%"}
+					borderRadius={"10px"}
+					border={"solid 1px #e7ebf2"}
+					bg={"#fff"}
+					mb={"15px"}
+				>
+					<Flex flexDir={"column"} alignItems={"center"} textAlign={"center"}>
+						<Text 
+							color={"#3d495d"} 
+							fontSize={"16px"} 
+							fontWeight={"bold"} 
+							mb={"10px"}
+						>
+							Tokamak Staking is transitioning to the Community Version.
+						</Text>
+						<Text 
+							color={"#86929d"} 
+							fontSize={"13px"} 
+							mb={"15px"}
+						>
+							Unstake & Withdraw are available on Community Version.
+						</Text>
+						<Link href={`https://community.staking.tokamak.network`} isExternal>
+							<Box
+								bg={"#2a72e5"}
+								color={"white"}
+								w={"180px"}
+								h={"36px"}
+								borderRadius={"8px"}
+								fontSize={"13px"}
+								fontWeight={500}
+								display={"flex"}
+								alignItems={"center"}
+								justifyContent={"center"}
+								cursor={"pointer"}
+								_hover={{ bg: "#1e5bb8" }}
+							>
+								Go to Community Version
+							</Box>
+						</Link>
 					</Flex>
-				) : selectedOp ? (
-					<WithdrawType
-						name={"Withdraw to Ethereum"}
-						content={
-							"Staked TON can be unstaked and can be withdrawn after 93,046 blocks from unstaking (~14 days)."
-						}
-						src={ETHEREUM.src}
-						onClick={() => openDrawer("Ethereum")}
-					/>
-				) : (
-					""
-				)}
+				</Box>
 			</Flex>
 			<WithdrawDrawer
 				onClose={onClose}
